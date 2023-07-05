ced78c0eb7fb3539aa75f7496da511a933346bb7
JC Guerrero <JCystems@gMail.com>
2023-07-05 10:18:31 -0500

    test(hero search.component) (#7)

--- src/modules/app/hero/components/search/hero-search.component.spec.ts
+++ src/modules/app/hero/components/search/hero-search.component.spec.ts
@@ -5,28 +5,126 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { HeroSearchComponent } from './hero-search.component';
+import { HeroAPI } from '../../types/hero-api';
+import { HeroService } from '../../service/hero.service';
+import { MockHeroService } from '../../../hero-mock/services/mock/hero.mock-service';
 
 describe('HeroSearchComponent', () => {
-  let component: HeroSearchComponent;
-  let fixture: ComponentFixture<HeroSearchComponent>;
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
+  let oHeroSearchComponent: HeroSearchComponent;
+  let oComponentFixture: ComponentFixture<HeroSearchComponent>;
+  let mockHeroService: HeroAPI;
+  let searchInput: HTMLInputElement;
+  let refresh: jest.SpyInstance;
+  let search: jest.SpyInstance;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [HeroSearchComponent],
+  describe('using MockHeroService', () => {
+    describe('search=""', () => {
+      beforeEach(() => {
+        TestBed.configureTestingModule({
+          imports: [HttpClientTestingModule],
+          declarations: [HeroSearchComponent],
+        })
+          .overrideComponent(HeroSearchComponent, {
+            set: {
+              providers: [{ provide: HeroService, useClass: MockHeroService }],
+            },
+          })
+          .compileComponents();
+
+        oComponentFixture = TestBed.createComponent(HeroSearchComponent);
+        mockHeroService =
+          oComponentFixture.debugElement.injector.get(HeroService);
+
+        oHeroSearchComponent = oComponentFixture.componentInstance;
+        refresh = jest.spyOn(oHeroSearchComponent, 'refresh');
+
+        oComponentFixture.detectChanges();
+      });
+
+      it('is instanceof HeroSearchComponent', () => {
+        expect(oHeroSearchComponent).toBeInstanceOf(HeroSearchComponent);
+      });
+
+      describe('Dependency Injection', () => {
+        describe('HeroService', () => {
+          it('is instanceof MockHeroService', () => {
+            expect(mockHeroService).not.toBeInstanceOf(HeroService);
+            expect(mockHeroService).toBeInstanceOf(MockHeroService);
+          });
+        });
+      });
+
+      describe('.refresh()', () => {
+        it('got invoked OnInit', () => {
+          expect(refresh).toHaveBeenCalled();
+        });
+      });
+
+      describe('HTML', () => {
+        describe('search input text box', () => {
+          it('renders "Hero Search"', () => {
+            const oHTMLElement = oComponentFixture.nativeElement //
+              .querySelector('#search-component');
+
+            expect(oHTMLElement.textContent).toContain('Hero Search');
+          });
+        });
+
+        it('matches snapshot', () => {
+          expect(oComponentFixture).toMatchSnapshot();
+        });
+      });
     });
-
-    httpClient = TestBed.inject(HttpClient);
-    httpTestingController = TestBed.inject(HttpTestingController);
-
-    fixture = TestBed.createComponent(HeroSearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('search="Celer"', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        declarations: [HeroSearchComponent],
+      })
+        .overrideComponent(HeroSearchComponent, {
+          set: {
+            providers: [{ provide: HeroService, useClass: MockHeroService }],
+          },
+        })
+        .compileComponents();
+
+      oComponentFixture = TestBed.createComponent(HeroSearchComponent);
+      mockHeroService =
+        oComponentFixture.debugElement.injector.get(HeroService);
+
+      oHeroSearchComponent = oComponentFixture.componentInstance;
+      search = jest.spyOn(oHeroSearchComponent, 'search');
+
+      searchInput =
+        oComponentFixture.nativeElement.querySelector('input#search-box');
+      searchInput.value = 'Celer';
+      searchInput.dispatchEvent(new Event('input'));
+
+      oComponentFixture.detectChanges();
+    });
+
+    describe('search(text)', () => {
+      it('gets invoked', () => {
+        expect(search).toHaveBeenCalledWith('Celer');
+      });
+    });
+
+    describe('HTML', () => {
+      // FIXME
+      xdescribe('.search-result', () => {
+        it('contains "Celeritas"', () => {
+          const oHTMLElement =
+            oComponentFixture.nativeElement.querySelector('.search-result');
+
+          expect(oHTMLElement.textContent).toContain('Celeritas');
+        });
+      });
+
+      it('matches snapshot', () => {
+        expect(oComponentFixture).toMatchSnapshot();
+      });
+    });
   });
 });
